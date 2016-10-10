41036b0e9081933678b96977aeeafff35399e405
Connor Nicol <connor.nicol@inps.co.uk>
2016-10-10 15:10:47 +0100

    added SQLite DB - save  serialised patient to DB

--- VSTOWordAddin2013And16/VSTOWordAddin2013And16/AddInUtilities.cs
+++ VSTOWordAddin2013And16/VSTOWordAddin2013And16/AddInUtilities.cs
@@ -5,6 +5,7 @@ using VSTOWordAddin.Core.Models;
 using VSTOWordAddin.Core.Interfaces;
 using Newtonsoft.Json;
 using Microsoft.Office.Interop.Word;
+using System.Data.SQLite;
 
 namespace VSTOWordAddin2013And16
 {
@@ -21,7 +22,24 @@ namespace VSTOWordAddin2013And16
 
             var currentRange = Globals.ThisAddIn.Application.Selection.Range;
             currentRange.Text = CurrentPatient.FamilyName + currentPatient.Age + CurrentPatient.GivenName + CurrentPatient.ID;
+
+            SQLiteConnection con = new SQLiteConnection(@"Data Source=E:\\DataBases\\VSTODB.db;Version=3;New=True;Compress=True;");
+            con.Open();
+
+            string sqlInsert = "insert into patient(SerialisedPatient) values ('" + PatData + "')";
+            var deleteCMD = new SQLiteCommand("DELETE FROM patient", con);
+            var CMD = new SQLiteCommand(sqlInsert, con);
+
+            try
+            {
+                deleteCMD.ExecuteNonQuery();
+                CMD.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+            }
         }
+
 
         public Patient CurrentPatient
         {
